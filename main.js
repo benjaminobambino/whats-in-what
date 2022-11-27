e4eeadb6018249358052ba4fc7ad173c42580e30
Benjamin Peck <benjaminlpeck@gmail.com>
2022-11-27 12:43:35 -0800

    add mama bear soy to comparisons

--- main.js
+++ main.js
@@ -4,23 +4,21 @@ const {
   enfamilNutramigen,
   parentsChoiceSensitivity,
   gerberGoodStartGentleSoy,
-  comfortsSoyBased
+  comfortsSoyBased,
+  mamaBearSoy
 } = require('./data');
 
 const { compareIngredients, getPrimaryOrSecondary } = require('./helpers');
 
-const allCompared = compareIngredients(
-  comfortsSoyBased,
-  gerberGoodStartGentleSoy
-);
+const allCompared = compareIngredients(mamaBearSoy, gerberGoodStartGentleSoy);
 
 const primaryCompared = compareIngredients(
-  getPrimaryOrSecondary(comfortsSoyBased, 0),
+  getPrimaryOrSecondary(mamaBearSoy, 0),
   getPrimaryOrSecondary(gerberGoodStartGentleSoy, 0)
 );
 
 const secondaryCompared = compareIngredients(
-  getPrimaryOrSecondary(comfortsSoyBased, 1),
+  getPrimaryOrSecondary(mamaBearSoy, 1),
   getPrimaryOrSecondary(gerberGoodStartGentleSoy, 1)
 );
 
